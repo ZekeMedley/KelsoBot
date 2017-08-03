d7035cf2febfa064da3bc0fa94b6d1434e0aec42
ZekeMedley <30676292+ZekeMedley@users.noreply.github.com>
2017-08-03 10:43:47 -0700

    Update kelso.py

--- kelso.py
+++ kelso.py
@@ -5,16 +5,20 @@ import tweepy
 from tweepy.api import API
 import random
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return 'Hello World!'
+# On Bluemix, get the port number from the environment variable VCAP_APP_PORT
+# When running this app on the local machine, default the port to 8080
+port = int(os.getenv('VCAP_APP_PORT', 8080))
 
-port = os.getenv('VCAP_APP_PORT', '8080')
-if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port))
+@app.route('/')
+def hello_world():
+    return 'Hello World! I am running on port ' + str(port)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
 
 kelsoChoices = ['making a deal', 'waiting and cooling off', 'going to another game', 'talking it out', 'sharing and taking turns', 'ignoring it', 'walking away', 'telling them to stop', 'apologizing']
 openings = ['Consider', 'Try', 'I\'d suggest', 'I\'d recommend', 'How about']
